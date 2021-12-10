10dafd5bb17c8cd9880bce56144b9937d9a11e17
blankRiot96 <veresharjun5@gmail.com>
2021-12-10 12:12:09 +0530

    Cleanup

--- pyhb/keyboard_sound_effects.py
+++ pyhb/keyboard_sound_effects.py
@@ -1,10 +1,12 @@
 import keyboard
 import os
+import random
 
 # Ignore the pygame welcome message
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
 import pygame
 import json
+
 
 # Initialize the pygame.mixer submodule
 pygame.mixer.init()
@@ -32,17 +34,36 @@ def main(sound_pack: str) -> None:
     global RELEASED
     print(f'pyhb has started playing {sound_pack}...')
     print('Use <ctrl + c> to close')
+
+
+    conf_vals = list(config['defines'].values())
+    conf_keys = list(config['defines'].keys())
+
+    session = {}
     while True:
-        for key in config['defines']:
-            value = config['defines'][key]
-            if value is None:
-                continue
-            if keyboard.is_pressed(value) and RELEASED:
+        key_pressed = keyboard.read_key()
+        if key_pressed in conf_vals:
+            if RELEASED:
                 if sound_pack == "nk-cream":
-                    sound = pygame.mixer.Sound(f"{user_path}/Soundpacks/{sound_pack}/{value}.wav")
-                    sound.play()
+                    sound = pygame.mixer.Sound(f"{user_path}/Soundpacks/{sound_pack}/{key_pressed}.wav")
                 else:
-                    sound = pygame.mixer.Sound(f"{user_path}/Soundpacks/{sound_pack}/{key}.ogg")
-                    sound.play()
-                RELEASED = False
+                    index = conf_vals.index(key_pressed)
+                    key = conf_keys[index]
+        else:
+            if key_pressed not in session:
+                value = conf_vals[random.randint(0, len(conf_vals) - 1)]
+                session[key_pressed] = value
+
+            key_pressed = session[key_pressed]
+            index = conf_vals.index(key_pressed)
+            key = conf_keys[index]
+
+        if RELEASED:
+            if sound_pack == "nk-cream":
+                sound = pygame.mixer.Sound(f"{user_path}/Soundpacks/{sound_pack}/{key_pressed}.wav")
+            else:
+                sound = pygame.mixer.Sound(f"{user_path}/Soundpacks/{sound_pack}/{key}.ogg")
+            sound.play()
+            RELEASED = False
+        
         keyboard.on_release(set_release)
---
+++ pyhb/typing_tester/settings.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+
+
+class Settings:
+    def __init__(self) -> None:
+        # Path to which 'pyhb' is installed 
+        self.user_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
+        print(self.user_path + '/settings_icon.png')
+        self.icon = pygame.image.load(self.user_path + '/settings_icon.png')
+
+    def update(self) -> None:
+        pass
+
+    def draw(self, screen) -> None:
+        screen.blit(self.icon, (0, 0))
+    
+
