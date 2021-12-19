5f06bf62fa282bdd66d60df2c48cb0bac414b158
blankRiot96 <veresharjun5@gmail.com>
2021-12-19 22:08:16 +0530

    Migrated completely to click

--- pyhb/__init__.py
+++ pyhb/__init__.py
@@ -3,67 +3,72 @@ import os
 import webbrowser
 import random
 from colorama import Fore, Style, Back
-from pyhb.utils import output
+from pyhb.utils import list_options, user_path
+from typing import Optional
+import sys
 
-
-COLORS = [
-	Fore.BLACK,
-	Fore.BLUE,
-	Fore.CYAN,
-	Fore.GREEN,
-	Fore.LIGHTBLACK_EX,
-	Fore.LIGHTBLUE_EX,
-	Fore.LIGHTCYAN_EX,
-	Fore.LIGHTGREEN_EX,
-	Fore.LIGHTMAGENTA_EX,
-	Fore.LIGHTRED_EX,
-	Fore.LIGHTYELLOW_EX,
-	Fore.MAGENTA,
-	Fore.RED,
-	Fore.RESET,
-	Fore.YELLOW
-]
-
-
-@click.group()
-@click.option('--play', is_flag=True, flag_value="", help="Lofi music to be played")
-@click.option('--typetest', flag_value="", help="Start an aesthetic typing test application")
-def cli():
-	pass
-
-
-@cli.command()
-def play_song(play):
-	global COLORS
-	songs = {
-	    "lofigirl": "https://www.youtube.com/watch?v=5qap5aO4i9A",
-	    "biscuit": "https://www.youtube.com/watch?v=EtZ2m2Zm3vY",
-	    "melancholy": "https://www.youtube.com/watch?v=RxglYGHuqFc",
-	    "street lights": "https://www.youtube.com/watch?v=FqXwkqfVGvA",
-	    "memory lane": "https://www.youtube.com/watch?v=6LXTuNDB160",
-	    "jiro dreams": "https://www.youtube.com/watch?v=sEYSpROMY5A",
-	    "*": "https://www.youtube.com/watch?v=EtZ2m2Zm3vY&list=PL6AyRhZu1p3KfZ56ToC0xZxIlpBLOsKXD",
-	}
-
-	if play:
-		webbrowser.open(songs[play])
-	else:
-		for index, song in enumerate(songs):
-			color = random.choice(COLORS)
-			output(color, f"[{index + 1}] {song}")
-
-		try:
-			promt = int(input('Choose a song number: '))
-			webbrowser.open(list(songs.values())[promt - 1])
-		except (ValueError, IndexError):
-			output(Fore.RED, "Invalid entry.")
-
-
-@cli.command()
-def type_test():
-	from pyhb.typing_tester import main 
-	main()
-
-
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
 def main():
-	cli()
+    pass
+
+
+@main.command(help="Play keyboard sound effects")
+@click.option('--soundpack', '-s', is_flag=False, flag_value="", help="Choose a soundpack")
+def start(soundpack: Optional[str]=None):
+    from pyhb.keyboard_sound_effects import main
+
+    if soundpack:
+        from pyhb.keyboard_sound_effects import main
+    else:
+        soundpacks = os.listdir(user_path + "/Soundpacks")
+        soundpacks.remove("config.json")
+
+        list_options(soundpacks)
+
+        promt = int(input("Choose a soundpack: "))
+        soundpack = soundpacks[promt - 1]
+
+    main(soundpack)
+
+
+@main.command(help="Install Soundpacks for pyhb")
+@click.option('--soundpacks', flag_value="")
+def install(soundpacks: str):
+    from pyhb.install_soundpacks import install
+    install(user_path)
+
+
+@main.command(help="Lofi music to be played")
+@click.option('--song', '-s', is_flag=False, flag_value="", help="Choose a song")
+def play(song: Optional[str]=None):
+    global COLORS
+    songs = {
+        "lofigirl": "https://www.youtube.com/watch?v=5qap5aO4i9A",
+        "biscuit": "https://www.youtube.com/watch?v=EtZ2m2Zm3vY",
+        "melancholy": "https://www.youtube.com/watch?v=RxglYGHuqFc",
+        "street lights": "https://www.youtube.com/watch?v=FqXwkqfVGvA",
+        "memory lane": "https://www.youtube.com/watch?v=6LXTuNDB160",
+        "jiro dreams": "https://www.youtube.com/watch?v=sEYSpROMY5A",
+        "*": "https://www.youtube.com/watch?v=EtZ2m2Zm3vY&list=PL6AyRhZu1p3KfZ56ToC0xZxIlpBLOsKXD",
+    }
+
+    if song:
+        webbrowser.open(songs[song])
+    else:
+        list_options(songs, colorize=True)
+
+        try:
+            promt = int(input('Choose a song number: '))
+            webbrowser.open(list(songs.values())[promt - 1])
+        except (ValueError, IndexError):
+            output(Fore.RED, "Invalid entry.")
+
+
+@main.command(help="Start an aesthetic typing test application")
+# @click.option('--typetest', flag_value="", help="Start an aesthetic typing test application")
+def typetest():
+    from pyhb.typing_tester import main 
+    main()
+
+
+
--- pyhb/utils.py
+++ pyhb/utils.py
@@ -1,7 +1,37 @@
 import click
+import random
+import os
 from colorama import Style
+from typing import List, Union
 
+
+user_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 
 def output(color, msg) -> None:
 	click.echo(color + msg + Style.RESET_ALL)
 
+
+def list_options(options: Union[List[str], dict], colorize: bool=False) -> None:
+	color = ""
+	if colorize:
+		colors = [
+		    Fore.BLUE,
+		    Fore.CYAN,
+		    Fore.GREEN,
+		    Fore.LIGHTBLACK_EX,
+		    Fore.LIGHTBLUE_EX,
+		    Fore.LIGHTCYAN_EX,
+		    Fore.LIGHTGREEN_EX,
+		    Fore.LIGHTMAGENTA_EX,
+		    Fore.LIGHTRED_EX,
+		    Fore.LIGHTYELLOW_EX,
+		    Fore.MAGENTA,
+		    Fore.RED,
+		    Fore.RESET,
+		    Fore.YELLOW
+		]
+		color = random.choice(colors)
+
+
+	for index, option in enumerate(options):
+		output(color, f"[{index + 1}] {option}")
