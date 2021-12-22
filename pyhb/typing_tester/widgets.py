0dec8f11dc50d437cf9e8b7f60d909665390e741
blankRiot96 <veresharjun5@gmail.com>
2021-12-22 22:27:14 +0530

    Added Punctual Toggle foundation

--- pyhb/typing_tester/widgets.py
+++ pyhb/typing_tester/widgets.py
@@ -1,4 +1,5 @@
 import pygame
+from typing import Tuple
 
 
 class Label:
@@ -48,3 +49,46 @@ class Label:
 
         screen.blit(self.surface, self.rect)
         screen.blit(self.t, self.t.get_rect(center=self.rect.center))
+
+
+class Toggle:
+    def __init__(self, size: Tuple[int, int]):
+        self.size = size
+
+        # Make rounded Surface
+        self.surf = pygame.Surface(size)
+        self.surf.set_colorkey((0, 0, 0))
+        self.grey = 100
+        self.color = pygame.Color((self.grey, self.grey, self.grey))
+        self.rect = pygame.Rect((0, 0), self.size)
+        self.pos_rect = pygame.Rect((0, 0), self.size)
+
+        # Flags
+        self.switch = False
+        self.transition = False
+
+        # Count variables
+        self.dt = 0
+
+    def update(self, dt):
+        self.dt = dt
+
+    def draw(self, screen: pygame.Surface, pos: Tuple[int, int]):
+        self.surf.fill((0, 0, 0))
+        # s_rect = screen.get_rect()
+
+        self.pos_rect.topleft = pos
+
+        pygame.draw.rect(self.surf, self.color, self.rect)
+        radius = self.size[1] // 2
+        pygame.draw.circle(screen, self.color, self.pos_rect.midleft, radius)
+        pygame.draw.circle(screen, self.color, self.pos_rect.midright, radius)
+
+        screen.blit(self.surf, pos)
+
+
+class ThemeSelection:
+    ...
+
+
+
